7b5adf40c6d01cbfd18c09fee9b731321b763335
danthe1st <daniel@wwwmaster.at>
2023-12-22 11:35:07 +0100

    remove unused import

--- src/main/java/io/github/danthe1st/spring_test/SpringTestApplication.java
+++ src/main/java/io/github/danthe1st/spring_test/SpringTestApplication.java
@@ -2,11 +2,10 @@ package io.github.danthe1st.spring_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class SpringTestApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
