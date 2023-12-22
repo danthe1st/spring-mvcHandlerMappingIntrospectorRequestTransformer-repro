98be89f19d3e74c8dbef43af6ab4dd93abf2d4a5
danthe1st <daniel@wwwmaster.at>
2023-12-22 11:18:30 +0100

    initial commit

---
+++ src/main/java/io/github/danthe1st/spring_test/SpringTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.danthe1st.spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+}
